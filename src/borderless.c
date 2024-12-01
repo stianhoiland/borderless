eb476f27a11783f51e112b6fdd3e9fe28424378c
Stian Gudmundsen Høiland <stianghoiland@gmail.com>
2024-12-02 00:31:35 +0100

    Use standard window proc for clarity, use compound literal for obscurity, learn about window closing and use WM_CLOSE instead of WM_DESTROY, use 0 instead of defines

--- src/borderless.c
+++ src/borderless.c
@@ -43,35 +43,31 @@ static LRESULT KeyboardProc(INT code, WPARAM wparam, LPARAM lparam)
 		!!(kbd->flags & LLKHF_ALTDOWN) && // alt is pressed
 		!!!(GetAsyncKeyState(kbd->vkCode) & 0x8000)) { // F11 key down is not a key repeat
 			ToggleBorderless(GetForegroundWindow());
-			return 1; // consume keyboard input
+			return 1; // consume F11 key down event
 	}
 	return CallNextHookEx(NULL, code, wparam, lparam);
 }
+static LRESULT WindowProc(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam)
+{
+	if (message == WM_CREATE) MessageBoxW(hwnd, L"Press Alt+F11 to toggle maximized borderless window.\nRe-launch borderless.exe to quit.", L"borderless", MB_OK | MB_ICONINFORMATION | MB_TASKMODAL);
+	if (message == WM_CLOSE) ToggleBorderless(0), PostQuitMessage(0);
+	return DefWindowProcW(hwnd, message, wparam, lparam);
+}
 INT wWinMain(HINSTANCE instance, HINSTANCE previnstance, WCHAR *args, INT show)
 {
-	static CONST WCHAR *class = L"borderlessAppClass";
+	static CONST WCHAR *class = L"borderlessGamingUtilityClass";
 	HANDLE mutex = CreateMutexW(NULL, 0, class);
 	if (GetLastError() == ERROR_ALREADY_EXISTS) {
 		if (MessageBoxW(NULL, L"Quit borderless?", L"borderless", MB_YESNO | MB_ICONQUESTION | MB_TASKMODAL) == IDYES) {
-			PostMessageW(FindWindowExW(HWND_MESSAGE, NULL, class, NULL), WM_DESTROY, NULL, NULL);
+			PostMessageW(FindWindowExW(HWND_MESSAGE, NULL, class, NULL), WM_CLOSE, 0, 0);
 		}
 		return 0;
 	}
-	WNDCLASSEX wcex = {0};
-	wcex.cbSize = sizeof wcex;
-	wcex.lpfnWndProc = DefWindowProcW;
-	wcex.hInstance = instance;
-	wcex.lpszClassName = class;
-	HWND hwnd = CreateWindowExW(0, RegisterClassExW(&wcex), NULL, 0, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, HWND_MESSAGE, NULL, instance, NULL);
+	HWND hwnd = CreateWindowExW(0, RegisterClassExW(&(WNDCLASSEXW){.cbSize = sizeof(WNDCLASSEXW), .lpfnWndProc = WindowProc, .hInstance = instance, .lpszClassName = class}), NULL, 0, 0, 0, 0, 0, HWND_MESSAGE, NULL, instance, NULL);
 	HHOOK hook = SetWindowsHookExW(WH_KEYBOARD_LL, (HOOKPROC)KeyboardProc, NULL, 0);
-	MessageBoxW(hwnd, L"Press Alt+F11 to toggle maximized borderless window.\nRe-launch borderless.exe to quit.", L"borderless", MB_OK | MB_ICONINFORMATION | MB_TASKMODAL);
 	MSG msg = {0};
-	while (GetMessageW(&msg, NULL, 0, 0) > 0) {
-		DispatchMessageW(&msg);
-		if (msg.message == WM_DESTROY) PostQuitMessage(0);
-	}
+	while (GetMessageW(&msg, hwnd, 0, 0) > 0) DispatchMessageW(&msg);
 	UnhookWindowsHookEx(hook);
-	ToggleBorderless(0); // undo before quitting
 	if (mutex) {
 		ReleaseMutex(mutex);
 	}
